6b3a84d7abe0a599f43dd5454a7fb5fae5e235ca
Marcin Wieczorek <marcin.wieczorek.programista@gmail.com>
2022-12-14 20:43:39 +0100

    Delete unnecessary try-catch scopes

--- App/Models/Expense.php
+++ App/Models/Expense.php
@@ -15,86 +15,64 @@ class Expense extends \Core\Model
 
     public static function getExpenseCategories($userId)
     {
-        try {
+        $db = static::getDB();
 
-            $db = static::getDB();
+        $statement = $db->prepare("SELECT id, name FROM expenses_category_assigned_to_users WHERE user_id = :userId");
+        $statement->bindValue(':userId', $userId, PDO::PARAM_INT);
+        $statement->execute();
 
-            $statement = $db->prepare("SELECT id, name FROM expenses_category_assigned_to_users WHERE user_id = :userId");
-            $statement->bindValue(':userId', $userId, PDO::PARAM_INT);
-            $statement->execute();
+        $expenseCategories = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-            $expenseCategories = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $expenseCategories;
 
-            return $expenseCategories;
-            
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
     }
 
     public static function getPaymentMethods($userId)
     {
-        try {
+        $db = static::getDB();
 
-            $db = static::getDB();
+        $statement = $db->prepare("SELECT id, name FROM payment_methods_assigned_to_users WHERE user_id = :userId");
+        $statement->bindValue(':userId', $userId, PDO::PARAM_INT);
+        $statement->execute();
 
-            $statement = $db->prepare("SELECT id, name FROM payment_methods_assigned_to_users WHERE user_id = :userId");
-            $statement->bindValue(':userId', $userId, PDO::PARAM_INT);
-            $statement->execute();
+        $paymentMethods = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-            $paymentMethods = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $paymentMethods;
 
-            return $paymentMethods;
-            
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
     }
 
     public static function getExpenses($userId, $startDate, $endDate)
     {
-        try {
+        $db = static::getDB();
 
-            $db = static::getDB();
+        $statement = $db->prepare("SELECT name AS categoryName, SUM(amount) AS categoryAmount FROM expenses, expenses_category_assigned_to_users 
+        WHERE expenses.user_id = :userId AND expenses.expense_category_assigned_to_user_id=expenses_category_assigned_to_users.id 
+        AND expenses.date_of_expense BETWEEN :startDate AND :endDate GROUP BY categoryName ORDER BY categoryAmount DESC");
+        $statement->bindValue(':userId', $userId, PDO::PARAM_INT);
+        $statement->bindValue(':startDate', $startDate, PDO::PARAM_STR);
+        $statement->bindValue(':endDate', $endDate, PDO::PARAM_STR);
+        $statement->execute();
 
-            $statement = $db->prepare("SELECT name AS categoryName, SUM(amount) AS categoryAmount FROM expenses, expenses_category_assigned_to_users 
-            WHERE expenses.user_id = :userId AND expenses.expense_category_assigned_to_user_id=expenses_category_assigned_to_users.id 
-            AND expenses.date_of_expense BETWEEN :startDate AND :endDate GROUP BY categoryName ORDER BY categoryAmount DESC");
-            $statement->bindValue(':userId', $userId, PDO::PARAM_INT);
-            $statement->bindValue(':startDate', $startDate, PDO::PARAM_STR);
-            $statement->bindValue(':endDate', $endDate, PDO::PARAM_STR);
-            $statement->execute();
+        $expenses = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-            $expenses = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-            return $expenses;
+        return $expenses;
             
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
     }
 
     public function addNewExpense($userId) 
     {
-        try {
+        $db = static::getDB();
 
-            $db = static::getDB();
-
-            $statement = $db->prepare("INSERT INTO expenses VALUES (NULL, :userId, :categoryId, :paymentMethodId, :amount, :date, :comment)");
-            $statement->bindValue(':userId', $userId, PDO::PARAM_INT);
-            $statement->bindValue(':categoryId', $this->categoryId, PDO::PARAM_INT);
-            $statement->bindValue(':paymentMethodId', $this->paymentMethodId, PDO::PARAM_INT);
-            $statement->bindValue(':amount', $this->amount, PDO::PARAM_STR);
-            $statement->bindValue(':date', $this->date, PDO::PARAM_STR);
-            $statement->bindValue(':comment', $this->comment, PDO::PARAM_STR);
+        $statement = $db->prepare("INSERT INTO expenses VALUES (NULL, :userId, :categoryId, :paymentMethodId, :amount, :date, :comment)");
+        $statement->bindValue(':userId', $userId, PDO::PARAM_INT);
+        $statement->bindValue(':categoryId', $this->categoryId, PDO::PARAM_INT);
+        $statement->bindValue(':paymentMethodId', $this->paymentMethodId, PDO::PARAM_INT);
+        $statement->bindValue(':amount', $this->amount, PDO::PARAM_STR);
+        $statement->bindValue(':date', $this->date, PDO::PARAM_STR);
+        $statement->bindValue(':comment', $this->comment, PDO::PARAM_STR);
+        
+        return $statement->execute();
             
-            return $statement->execute();
-            
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
-
-        return false;
     }
 
 }
--- App/Models/Income.php
+++ App/Models/Income.php
@@ -15,66 +15,49 @@ class Income extends \Core\Model
 
     public static function getIncomeCategories($userId)
     {
-        try {
+        $db = static::getDB();
 
-            $db = static::getDB();
+        $statement = $db->prepare("SELECT id, name FROM incomes_category_assigned_to_users WHERE user_id = :userId");
+        $statement->bindValue(':userId', $userId, PDO::PARAM_INT);
+        $statement->execute();
 
-            $statement = $db->prepare("SELECT id, name FROM incomes_category_assigned_to_users WHERE user_id = :userId");
-            $statement->bindValue(':userId', $userId, PDO::PARAM_INT);
-            $statement->execute();
+        $incomeCategories = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-            $incomeCategories = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-            return $incomeCategories;
+        return $incomeCategories;
             
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
     }
 
     public static function getIncomes($userId, $startDate, $endDate)
     {
-        try {
+        $db = static::getDB();
 
-            $db = static::getDB();
+        $statement = $db->prepare("SELECT name AS categoryName, SUM(amount) AS categoryAmount FROM incomes, incomes_category_assigned_to_users 
+        WHERE incomes.user_id = :userId AND incomes.income_category_assigned_to_user_id = incomes_category_assigned_to_users.id 
+        AND incomes.date_of_income BETWEEN :startDate AND :endDate GROUP BY categoryName ORDER BY categoryAmount DESC");
+        $statement->bindValue(':userId', $userId, PDO::PARAM_INT);
+        $statement->bindValue(':startDate', $startDate, PDO::PARAM_STR);
+        $statement->bindValue(':endDate', $endDate, PDO::PARAM_STR);
+        $statement->execute();
 
-            $statement = $db->prepare("SELECT name AS categoryName, SUM(amount) AS categoryAmount FROM incomes, incomes_category_assigned_to_users 
-            WHERE incomes.user_id = :userId AND incomes.income_category_assigned_to_user_id = incomes_category_assigned_to_users.id 
-            AND incomes.date_of_income BETWEEN :startDate AND :endDate GROUP BY categoryName ORDER BY categoryAmount DESC");
-            $statement->bindValue(':userId', $userId, PDO::PARAM_INT);
-            $statement->bindValue(':startDate', $startDate, PDO::PARAM_STR);
-            $statement->bindValue(':endDate', $endDate, PDO::PARAM_STR);
-            $statement->execute();
+        $incomes = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-            $incomes = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $incomes;
 
-            return $incomes;
-            
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
     }
 
     public function addNewIncome($userId)
     {
-        try {
+        $db = static::getDB();
 
-            $db = static::getDB();
-
-            $statement = $db->prepare("INSERT INTO incomes VALUES (NULL, :userId, :categoryId, :amount, :date, :comment)");
-            $statement->bindValue(':userId', $userId, PDO::PARAM_INT);
-            $statement->bindValue(':categoryId', $this->categoryId, PDO::PARAM_INT);
-            $statement->bindValue(':amount', $this->amount, PDO::PARAM_STR);
-            $statement->bindValue(':date', $this->date, PDO::PARAM_STR);
-            $statement->bindValue(':comment', $this->comment, PDO::PARAM_STR);
+        $statement = $db->prepare("INSERT INTO incomes VALUES (NULL, :userId, :categoryId, :amount, :date, :comment)");
+        $statement->bindValue(':userId', $userId, PDO::PARAM_INT);
+        $statement->bindValue(':categoryId', $this->categoryId, PDO::PARAM_INT);
+        $statement->bindValue(':amount', $this->amount, PDO::PARAM_STR);
+        $statement->bindValue(':date', $this->date, PDO::PARAM_STR);
+        $statement->bindValue(':comment', $this->comment, PDO::PARAM_STR);
+        
+        return $statement->execute();
             
-            return $statement->execute();
-            
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
-
-        return false;
     }
 }
 
