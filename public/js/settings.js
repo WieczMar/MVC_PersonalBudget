51af8f9f917fd47d83fff67912b548d49ba1c44b
Marcin Wieczorek <marcin.wieczorek.programista@gmail.com>
2023-02-27 19:05:15 +0100

    If expense monthly limit is set, it will show up on categories list below expense name

--- public/js/settings.js
+++ public/js/settings.js
@@ -146,24 +146,38 @@ const renderItemsInList = (key, list, data) => {
   data.forEach((categoryOrMethod) => {
     let newListItem = document.createElement("li");
     newListItem.setAttribute("class", "list-group-item list-group-item-action d-flex dynamicListItem");
-    let innerHTML = `
-        <div class="col-6">
+
+    if((key === "expense-category")&&(categoryOrMethod.monthly_limit != null)){
+      var innerHTMLCategoryName = `
+        <div class="col-8 col-sm-6">
         ${categoryOrMethod.name}
+        <div class="text-secondary">
+          Monthly limit: ${categoryOrMethod.monthly_limit}
         </div>
-        <div class="col-6 d-flex justify-content-end align-items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square edit-icon clickable-icon me-2 ${key}-edit-icon" viewBox="0 0 16 16" 
-            id="${key}:${categoryOrMethod.id}:${categoryOrMethod.name}">
-                <path style="pointer-events: none;" d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                <path style="pointer-events: none;" fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square text-danger clickable-icon delete-icon" viewBox="0 0 16 16"
-            id="${key}:${categoryOrMethod.id}:${categoryOrMethod.name}">
-                <path style="pointer-events: none;" d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                <path style="pointer-events: none;" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-              </svg>
         </div>`;
-      newListItem.innerHTML = innerHTML;
-      list.appendChild(newListItem);
+    } else {
+      var innerHTMLCategoryName = `
+      <div class="col-8 col-sm-6">
+      ${categoryOrMethod.name}
+      </div>`;
+    };
+
+    const innerHTMLIcons = `
+    <div class="col-4 col-sm-6 d-flex justify-content-end align-items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square edit-icon clickable-icon me-2 ${key}-edit-icon" viewBox="0 0 16 16" 
+        id="${key}:${categoryOrMethod.id}:${categoryOrMethod.name}">
+            <path style="pointer-events: none;" d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+            <path style="pointer-events: none;" fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square text-danger clickable-icon delete-icon" viewBox="0 0 16 16"
+        id="${key}:${categoryOrMethod.id}:${categoryOrMethod.name}">
+            <path style="pointer-events: none;" d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+            <path style="pointer-events: none;" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+          </svg>
+    </div>`;
+    
+    newListItem.innerHTML = (innerHTMLCategoryName + innerHTMLIcons);
+    list.appendChild(newListItem);
     });
 };
 
